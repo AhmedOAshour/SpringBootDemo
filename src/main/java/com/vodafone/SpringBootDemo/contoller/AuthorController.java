48f4328474926bccfde26d3f4bdb98008b994c91
Ahmed Ashour <ahmed.o.ashour1@gmail.com>
2023-05-23 19:36:43 +0300

    use MediaType in AuthorController

--- src/main/java/com/vodafone/SpringBootDemo/contoller/AuthorController.java
+++ src/main/java/com/vodafone/SpringBootDemo/contoller/AuthorController.java
@@ -4,6 +4,7 @@ import com.vodafone.SpringBootDemo.model.Author;
 import com.vodafone.SpringBootDemo.service.AuthorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,27 +17,27 @@ public class AuthorController {
     @Autowired
     private AuthorService authorService;
 
-    @GetMapping(value = "/authors", produces = {"application/json"})
+    @GetMapping(value = "/authors", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<Author>> getAuthors(){
         return ResponseEntity.ok(authorService.getAllAuthors(0, 0));
     }
 
-    @GetMapping(value = "/authors/{id}", produces = {"application/json"})
+    @GetMapping(value = "/authors/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Author> getAuthorById(@PathVariable(name = "id") Integer id){
         return ResponseEntity.ok(authorService.getAuthorById(id));
     }
 
-    @PostMapping(value = "authors", produces = {"application/json"}, consumes = {"application/json"})
+    @PostMapping(value = "authors", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Author> addAuthor(@RequestBody Author author){
         return ResponseEntity.ok(authorService.addAuthor(author));
     }
 
-    @PutMapping(value = "/authors/{id}", produces = {"application/json"}, consumes = {"application/json"})
+    @PutMapping(value = "/authors/{id}", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Author> updateAuthor(@PathVariable(name = "id") Integer id, @RequestBody Author author) {
         return new ResponseEntity<Author>(authorService.updateAuthor(id, author), HttpStatus.CREATED);
     }
 
-    @DeleteMapping(value = "/authors/{id}", produces = {"application/json"})
+    @DeleteMapping(value = "/authors/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Author> deleteAuthor(@PathVariable(name = "id") Integer id) {
         authorService.deleteAuthor(id);
         return new ResponseEntity(HttpStatus.NO_CONTENT);
